b6c4c4bcc447eed18dbef7084ddeb786f44837b9
Anton <ant.punko@gmail.com>
2016-04-30 20:58:28 +0300

    Edited assets for project needs

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -1,9 +1,38 @@
 $(function() {
     debugger;
+    setFormElements();
+});
+
+function setFormElements() {
+    setMultiSelectInputs();
+    setDateInputs();
+}
+
+function setMultiSelectInputs(){
     $('.multiSelect').fSelect();
-    var elements = $('.fs-label');
-    elements[0].innerHTML = "Select People";
+    $('.fs-label').innerHTML = "Select People";
+}
+
+function setDateInputs(){
+    setNewDateInputs();
+    setEditDateInputs();
+}
+
+function setNewDateInputs(){
     var date = new Date();
     var dateString =  (date.getYear() + 1900) + "-0" + (date.getMonth() + 1) + "-" + date.getDate();
     $('.newDateInput').val(dateString);
-});
+}
+
+function setEditDateInputs(){
+    var dateInputs = $('.editDateInput');
+    for (var i = 0; i<dateInputs.length; i++){
+        var date = dateInputs[i];
+        $(dateInputs[i]).val(convertDate($(date).val()));
+    }
+}
+
+function convertDate(date){
+    var convertedDate = date.substring(6,10) + "-" + date.substring(3,5) + "-" + date.substring(0, 2);
+    return convertedDate;
+}
